4251a40b332f58e866c2f5f816adf383870327c0
Shadowwolf <58004180+Shadowwolf0621@users.noreply.github.com>
2019-11-21 10:11:02 -0800

    Test Canvas (Not Working Yet)

--- TestCanvas.php
+++ TestCanvas.php
@@ -5,11 +5,42 @@
 <title>Untitled Document</title>
 </head>
 
-<body>
+	
+<body style = 'background-color: #DFDFDF'>
 	
 	<div style='display: flex; justify-content: center; align-content: center;'>
-		<canvas id='PlannerCanvas' style='width:50vw; height:50vw;'></canvas>
+		<canvas id='PlannerCanvas' style='width:50vw; height:50vw; border: 1px solid black' onClick='ClickOnCanvas();' onMouseMove='GetMousePos(e);'></canvas>
 	</div>
 	
 </body>
+	
+	<script>
+		
+		var clickPos;
+		var canvas = document.getElementById('PlannerCanvas');
+		var ctx = canvas.getContext('2d');
+		var w = canvas.width;
+		var h = canvas.height;
+		var MouseX;
+		var MouseY;	
+		
+		function ClickOnCanvas(){
+			
+			GetMousePos();
+			
+		}
+		
+		function GetMousePos(e){
+			
+			var rect = canvas.getBoundingClientRect();
+	
+			MouseX = e.clientX - rect.left;
+			MouseY = e.clientY - rect.top;
+			
+			window.alert(MouseX);
+		}
+		
+		
+	</script>
+	
 </html>
