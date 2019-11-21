2ee62eb27b7486be3b3003d3d02b1af8e76f73b8
Shadowwolf <58004180+Shadowwolf0621@users.noreply.github.com>
2019-11-21 09:28:13 -0800

    Canvas

---
+++ TestCanvas.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+	
+	<div style='display: flex; justify-content: center; align-content: center;'>
+		<canvas id='PlannerCanvas' style='width:50vw; height:50vw;'></canvas>
+	</div>
+	
+</body>
+</html>
